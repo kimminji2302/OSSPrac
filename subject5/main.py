ce82fb37f21c90c450c7843e5c4ebffc0e342bc1
minji <kimminji2302@gmail.com>
2026-04-09 14:34:21 +0900

    ossprac1: 샘플 동작 코드 수정 완료

--- subject5/main.py
+++ subject5/main.py
@@ -1,8 +1,15 @@
-name = input("이름을 입력하세요: ")
-student_id = input("학번을 입력하세요: ")
-major = input("학과를 입력하세요: ")
+# 사용자 정보 입력 프로그램
 
-print("\n<출력>")
-print("이름:", name)
-print("학번:", student_id)
-print("학과:", major)
+# 사용자에게 정보를 입력 받음
+name = input("이름을 입력하세요 : ")
+student_id = input("학번을 입력하세요 : ")
+major = input("학과를 입력하세요 : ")
+school = input("학교를 입력하세요 : ")
+
+# 입력받은 정보를 출력
+print("\n입력하신 정보는 다음과 같습니다:")
+print(f"이름: {name}")
+print(f"학번: {student_id}")
+print(f"학과: {major}")
+print(f"학교: {school}")
+
